97367657f03b9dc229ffc22f2b331bf464760a6b
pamirian <mirzoshoev@gmail.com>
2022-04-04 21:55:31 +0200

    aStringAndATwoChars solved

--- 29-03-2022/src/com/company/aStringAndATwoChars.java
+++ 29-03-2022/src/com/company/aStringAndATwoChars.java
@@ -2,9 +2,16 @@ package com.company;
 
 public class aStringAndATwoChars {
     public static void main(String[] args) {
-        strObr("Coming soon...", 'a', 'b');
+       strObr("more expresso - less depresso ", 'x', 's');
     }
-
     public static void strObr(String str, char a, char b) {
+        for (int i=0; i<str.length(); i++) {
+
+            if (str.charAt(i) == a) {
+                System.out.print(b);
+            } else {
+                System.out.print(str.charAt(i));
+            }
+        }
     }
 }
