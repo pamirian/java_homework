e10a0506cf859771e8db6e8a62fd4c937c28d708
pamirian <mirzoshoev@gmail.com>
2022-04-04 22:01:06 +0200

    aStringAndATwoChars solved

--- 29-03-2022/src/com/company/aStringAndATwoChars.java
+++ 29-03-2022/src/com/company/aStringAndATwoChars.java
@@ -1,12 +1,14 @@
 package com.company;
-
+/*
+Write a method that takes a String and a two chars. As the result it returns the given String, where each the
+first char was replaced by the second char. You have to use only length() and charAt() methods of String class.
+ */
 public class aStringAndATwoChars {
     public static void main(String[] args) {
        strObr("more expresso - less depresso ", 'x', 's');
     }
     public static void strObr(String str, char a, char b) {
         for (int i=0; i<str.length(); i++) {
-
             if (str.charAt(i) == a) {
                 System.out.print(b);
             } else {
