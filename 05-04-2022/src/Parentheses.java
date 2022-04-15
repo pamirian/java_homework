91da82f32c6131911dd78a7c00a7d062a72afe22
pamirian <mirzoshoev@gmail.com>
2022-04-16 00:20:49 +0200

    Parentheses added

--- 05-04-2022/src/Parentheses.java
+++ 05-04-2022/src/Parentheses.java
@@ -6,7 +6,27 @@ Try to solve it yourself without help of Google
 “98+3)-( (9+1)*5” -> false
  */
 public class Parentheses {
-    public static void main(String[] args) {
-        System.out.println("Coming soon...");
+        public static void main(String[] args) {
+            System.out.println("\"9*(8+3)-( (9+1)5)\" -> "+Parentheses("9*(8+3)-( (9+1)5)"));
+            System.out.println("\"9(8+3)- (9+1)5)\" -> "+Parentheses("9(8+3)- (9+1)5)"));
+            System.out.println("\"98+3)-( (9+1)*5\" -> "+Parentheses("98+3)-( (9+1)*5"));
+        }
+        public static Boolean Parentheses(String str) {
+            boolean result=false;
+            int parenthesOpenCounter = 0;
+            int parenthesClosedCounter = 0;
+            for (int i = 0; i < str.length(); i++) {
+                char currentSymbol = str.charAt(i);
+                if(currentSymbol=='('){
+                    parenthesOpenCounter = parenthesOpenCounter + 1;
+                }
+                if(currentSymbol==')'){
+                    parenthesClosedCounter = parenthesClosedCounter + 1;
+                }
+            }
+            if (parenthesOpenCounter-parenthesClosedCounter==0){
+                result=true;
+            }
+            return result;
+        }
     }
-}
