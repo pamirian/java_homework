450e7d11ccf2203d89fe5f387eebff190b33768b
pamirian <mirzoshoev@gmail.com>
2022-08-02 01:54:40 +0200

    homework 01-08-2022

---
+++ 01-08-2022/src/com/company/Employee.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Employee {
+    String name, jobTitle, email, phone;
+    int salary;
+    int age;
+
+    // Параметризированный конструктор
+    Employee(String name, String jobTitle, String email, String phone, int salary, int age) {
+        this.name = name;
+        this.jobTitle = jobTitle;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    // метод, который выводит информацию об объекте в консоль.
+    void getEmployeeInfo() {
+        if (age > 40) {
+            System.out.println("Name: " + this.name + ", Position: " + this.jobTitle + ", Email: " + this.email + ", Phone:" + this.phone + ", Salary: " + salary + ", Age: " + age);
+        }
+    }
+
+}
