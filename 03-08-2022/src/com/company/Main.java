6d69a74bcd85b4711e4e1e2e898c5797837d4d66
pamirian <mirzoshoev@gmail.com>
2022-08-08 03:29:09 +0200

    homework refactored

--- 03-08-2022/src/com/company/Main.java
+++ 03-08-2022/src/com/company/Main.java
@@ -6,40 +6,43 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Point point1 = new Point();
-        point1.printPoint();
-
-        System.out.println("-----");
-
+// кейс 1 Когда значения цветов заданы вне диапазона, то выводится ошибка и они сеттерами приводятся к границам диапазона
         Point point2 = new Point();
+
         point2.setCoordX(1);
         point2.setCoordY(1);
         point2.setColorR(-1);
         point2.setColorG(290);
         point2.setColorB(1);
 
-        point2.printPoint();
+            point2.printPoint();
 
         System.out.println("-----");
 
-        // не понимаю, как сделать так чтобы point3 с неправильными цветами выдавал ошибку
-        // почеиу-то сеттеры не меняют значения из конструктора
+// кейс 2 Когда цвета и координаты не заданы, конструктором по умолчанию ставит точку черного цвета в начале координат
+        Point point1 = new Point();
+        point1.printPoint();
+
+        System.out.println("-----");
+
+// кейс 3 Если ошибочные параметры цветов введены изначально, они просто выводятся. Не реализована проверка в конструкторе
         Point point3 = new Point(1,1,-2,-2,-2);
         point3.printPoint();
 
         System.out.println("-----");
 
-        // ввод параметров с консоли, но тут тоже сеттеры не меняют значения из конструктора если ввести цвета из неправильного диапазона
+// кейс 4 Ввод параметров с консоли. Если ошибочные параметры цветов введены изначально, они также просто выводятся.
+// также не реализована проверка в конструкторе в силу того что это было бы не правильно. а как это сделать не понятно((
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите координаты точки в формате:[X,Y,R,G,B], где:\nX-координаты точки по оси X\nY-координаты точки по оси Y\nR-первый компанент цвета точки\nG-второй компанент цвета точки\nB-третий компанент цвета точки\n>>");
-            int coordX = sc.nextInt();
-            int coordY = sc.nextInt();
-            int colorR = sc.nextInt();
-            int colorG = sc.nextInt();
-            int colorB = sc.nextInt();
+        int coordX = sc.nextInt();
+        int coordY = sc.nextInt();
+        int colorR = sc.nextInt();
+        int colorG = sc.nextInt();
+        int colorB = sc.nextInt();
 
-            Point point0 = new Point(coordX, coordY, colorR, colorG, colorB);
-            point0.printPoint();
+        Point point0 = new Point(coordX, coordY, colorR, colorG, colorB);
+        point0.printPoint();
 
     }
 }
--- 03-08-2022/src/com/company/Point.java
+++ 03-08-2022/src/com/company/Point.java
@@ -6,7 +6,7 @@ public class Point {
     private int colorR;
     private int colorG;
     private int colorB;
-//Конструктор без параметров, по умолчанию ставит точку черного цвета в начале координат
+    //Конструктор без параметров, по умолчанию ставит точку черного цвета в начале координат
     public Point(){
         this.coordX = 0;
         this.coordY = 0;
@@ -15,17 +15,13 @@ public class Point {
         this.colorB = 0;
     }
 
-//Конструктор с параметрами
+    //Конструктор с параметрами
     public Point(int coordX, int coordY, int colorR, int colorG, int colorB) {
         this.coordX = coordX;
         this.coordY = coordY;
         this.colorR = colorR;
         this.colorG = colorG;
         this.colorB = colorB;
-    }
-
-    public void printPoint(){
-        System.out.println("Координаты точки: [x="+coordX + ", y=" +coordY + "], цвет: ("+colorR+","+colorG+","+colorB+")");
     }
 
     public void setCoordX(int coordX) {
@@ -37,38 +33,48 @@ public class Point {
     }
 
     public void setColorR(int colorR) {
-        if(colorR<0){System.out.println("Ошибка! B-компонент цвета ниже допустимого диапазона");
+        if(colorR<0){
+            System.err.println("Ошибка! R-компонент цвета ниже допустимого диапазона");
             this.colorR = 0;
-            return;}
-        if(colorR>255){System.out.println("Ошибка! B-компонент цвета выше допустимого диапазона");
+            } else
+        if(colorR>255){
+            System.err.println("Ошибка! R-компонент цвета выше допустимого диапазона");
             this.colorR = 255;
-            return;}
+            }
         else {
             this.colorR = colorR;
         }
     }
 
     public void setColorG(int colorG) {
-        if(colorG<0){System.out.println("Ошибка! B-компонент цвета ниже допустимого диапазона");
+        if(colorG<0){
+            System.err.println("Ошибка! G-компонент цвета ниже допустимого диапазона");
             this.colorG = 0;
-            return;}
-        if(colorG>255){System.out.println("Ошибка! B-компонент цвета выше допустимого диапазона");
+            } else
+        if(colorG>255){
+            System.err.println("Ошибка! G-компонент цвета выше допустимого диапазона");
             this.colorG = 255;
-            return;}
+            }
         else {
             this.colorG = colorG;
         }
     }
 
     public void setColorB(int colorB) {
-        if(colorB<0){System.out.println("Ошибка! B-компонент цвета ниже допустимого диапазона");
+        if(colorB<0){
+            System.err.println("Ошибка! B-компонент цвета ниже допустимого диапазона");
             this.colorB = 0;
-            return;}
-        if(colorB>255){System.out.println("Ошибка! B-компонент цвета выше допустимого диапазона");
+            } else
+        if(colorB>255){
+            System.err.println("Ошибка! B-компонент цвета выше допустимого диапазона");
             this.colorB = 255;
-            return;}
+            }
         else {
             this.colorB = colorB;
         }
+    }
+
+    public void printPoint(){
+        System.out.println("Координаты точки: [x="+coordX + ", y=" +coordY + "], цвет: ("+colorR+","+colorG+","+colorB+")");
     }
 }
