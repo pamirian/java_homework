ded5cb24b69700457fbbc44a2572b4ea92feed95
pamirian <mirzoshoev@gmail.com>
2022-09-10 01:48:04 +0200

    Comparable

--- 07-09-2022/src/com/company/Soldier.java
+++ 07-09-2022/src/com/company/Soldier.java
@@ -1,2 +1,24 @@
-package com.company;public class Soldier {
+package com.company;
+
+
+class Soldier implements Comparable<Soldier> {
+    private final Integer height;
+
+    public Soldier(Integer height) {
+        this.height = height;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    @Override
+    public String toString() {
+        return  "height=" + height;
+    }
+
+    @Override
+    public int compareTo(Soldier o) {
+        return -1 * (this.getHeight()-o.getHeight());
+    }
 }
